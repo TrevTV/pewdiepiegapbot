0390ba73bc00dd87d007b863c0dba1b884defd00
Trevor Jackson <trevorj342@gmail.com>
2019-06-07 18:17:52 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 from discord.ext import commands
 from config import Config
+import cogs.utils
 import discord
 
 
 cfg = Config(open('config.cfg'))
 client = commands.Bot(command_prefix=cfg.prefix)
-extensions = ['fun', 'utils']
+extensions = ['cogs.fun', 'cogs.utils', 'cogs.pewdsb']
 
-"""
+
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         embed=discord.Embed(title="Incorrect Usage", description="Please use !help to find out how to use that command.", color=0xF9013F)
         await ctx.send(embed=embed)
-"""
+
 
 @client.event
 async def on_ready():
